e331beaaf07f6739eb5000600fb817d70f4ad700
神霊種 <victor.dalacorte@etec.sp.gov.br>
2019-11-10 23:36:12 +0000

    Java

---
+++ exception.java
@@ -0,0 +1,10 @@
+public class exception {
+    public static void main(String[] args) {
+
+        try {
+        }
+            catch(Exception e) {
+                System.out.println("Hello World");
+        }
+    }
+}
