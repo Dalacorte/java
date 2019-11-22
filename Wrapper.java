d6f8799e7f7d87009b2a93f4802565865770139f
神霊種 <victor.dalacorte@etec.sp.gov.br>
2019-11-22 21:30:03 +0000

    Java

---
+++ Wrapper.java
@@ -0,0 +1,12 @@
+public class Wrapper {
+    public static void main (String args[]) {
+
+        Integer i = 10;
+        Double d = 10.5;
+        Byte b = 1;
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(b);
+    }
+}
