9c8c47706392a8362caad8fafaa3827ca2e2e2e5
神霊種 <victor.dalacorte@etec.sp.gov.br>
2019-11-28 19:01:32 +0000

    Java

---
+++ Lambda.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class File {
+    public static void main(String[] args) {
+
+        (int a, int b) -> {  return a + b; }
+
+        () -> System.out.println("Hello World");
+        
+        (String s) -> { System.out.println(s); }
+    }
+}
